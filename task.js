bb42148381fd92782efd336a0cbf09a5bdb272a2
Zandr Martin <git@zandr.me>
2016-12-17 10:52:22 -0600

    fix a bunch of stuff and break into separate files

---
+++ task.js
@@ -0,0 +1,99 @@
+// Copyright © 2016 Zandr Martin
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"use strict";
+const path = require("path");
+const fs = require("fs");
+const DATA_FILE = (typeof process.env.XDG_DATA_HOME === "undefined")
+                  ? path.join(process.env.HOME, ".tasks.json")
+                  : path.join(process.env.XDG_DATA_HOME, "tasks.json");
+
+const Task = {
+    objProto: {
+        id: null,
+        name: null,
+        _due: null,
+        get due() { return this._due; },
+        set due(d) {
+            if (d) {
+                this._due = (typeof d === "object") ? d : new Date(d);
+            }
+        },
+        recurs: false,
+        schedule: null,
+        completed: false,
+        complete: function () {
+            if (this.recurs && this.schedule) {
+                // TODO: change due date here
+            } else {
+                this.completed = true;
+            }
+        },
+        tags: [],
+        toJSON: function () {
+            let ret = {
+                id: this.id,
+                name: this.name,
+                recurs: this.recurs,
+                completed: this.completed
+            };
+
+            if (this.due) {
+                ret.due = this.due;
+            }
+
+            if (this.tags.length > 0) {
+                ret.tags = this.tags;
+            }
+
+            return ret;
+        }
+    },
+    new: function (init = {}) {
+        let t = Object.create(Task.objProto);
+        t.id = 0;
+
+        while (Task.registry.items.filter((item) => item.id === t.id).length > 0) {
+            // set id to first unused id
+            t.id++;
+        }
+
+        Object.assign(t, init);
+        Task.registry.add(t);
+        return t;
+    },
+    registry: {
+        items: [],
+        tags: new Set(),
+        add: function (item) {
+            this.items.push(item);
+            item.tags.forEach((tag) => this.tags.add(tag));
+        },
+        save: function () {
+            fs.writeFileSync(DATA_FILE, JSON.stringify(this.items), { mode: 0o644 });
+        },
+        load: function () {
+            let data = JSON.parse(fs.readFileSync(DATA_FILE));
+            if (data && data.length) {
+                data.forEach((t) => Task.new(t));
+            }
+        },
+        remove: function (id) {
+            let i = this.items.findIndex((t) => t.id === id);
+            return this.items.splice(i, 1);
+        }
+    }
+};
+
+module.exports = Task;
--- tasks.js
+++ tasks.js
@@ -13,12 +13,9 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 "use strict";
-const path = require("path");
-const fs = require("fs");
+require("./polyfills.js");
+const Task = require("./task.js");
 const console = require("console");
-const DATA_FILE = (typeof process.env.XDG_DATA_HOME === "undefined")
-                  ? path.join(process.env.HOME, ".tasks.json")
-                  : path.join(process.env.XDG_DATA_HOME, "tasks.json");
 const DAYS = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 const ACTIONS = ["add", "complete", "delete", "list", "status"];
 const USAGE = `Usage: node tasks.js <action> <options>
@@ -37,93 +34,6 @@ const USAGE = `Usage: node tasks.js <action> <options>
     -d, --due <date|day-of-week>
     -r, --recurs <#> <day|week|month|year>
     -t, --tags tag1 [tag2 tag3 ...]`;
-
-if (!String.prototype.isNumeric) {
-    String.prototype.isNumeric = function () {
-        return !isNaN(parseInt(this, 10));
-    };
-}
-
-if (!String.prototype.padRight) {
-    String.prototype.padRight = function (amount, pad = " ") {
-        let newString = this.toString();
-        while (newString.length < amount) {
-            newString += pad;
-        }
-        return newString;
-    };
-}
-
-if (!String.prototype.padLeft) {
-    String.prototype.padLeft = function (amount, pad = " ") {
-        let newString = this.toString();
-        while (newString.length < amount) {
-            newString = pad + newString;
-        }
-        return newString;
-    };
-}
-
-function Task(init = {}) {
-    let ret = {
-        id: Task.nextId(),
-        name: null,
-        _due: null,
-        get due() { return this._due; },
-        set due(d) {
-            if (d) {
-                this._due = (typeof d === "object") ? d : new Date(d);
-            }
-        },
-        recurs: false,
-        schedule: null,
-        completed: false,
-        complete: function () {
-            if (this.recurs && this.schedule) {
-                // change due date here
-            } else {
-                this.completed = true;
-            }
-        },
-        tags: []
-    };
-
-    Object.assign(ret, init);
-    Task.registry.add(ret);
-    return ret;
-}
-
-Task.nextId = function () {
-    let ret = 0;
-
-    while (Task.registry.items.filter((item) => item.id === ret).length > 0) {
-        ret++;
-    }
-
-    return ret;
-};
-
-Task.registry = {
-    items: [],
-    tags: new Set(),
-    add: function (item) {
-        this.items.push(item);
-        item.tags.forEach((tag) => this.tags.add(tag));
-    },
-    save: function () {
-        fs.writeFileSync(DATA_FILE, JSON.stringify(this.items), { mode: 0o644 });
-    },
-    load: function () {
-        let data = JSON.parse(fs.readFileSync(DATA_FILE));
-        if (data && data.length) {
-            data.forEach((t) => Task(t));
-        }
-    },
-    remove: function (id) {
-        let i = this.items.findIndex((t) => t.id === id);
-        return this.items.splice(i, 1);
-    }
-};
 
 Task.registry.load();
 
@@ -145,14 +55,16 @@ function parseDueDate(_dt) {
 
     d = new Date();
 
-    if (dt === "tomorrow") {
+    if (dt === "today") {
+        d.setDate(now.getDate());
+    } else if (dt === "tomorrow") {
         d.setDate(now.getDate() + 1);
     } else if (DAYS.includes(dt)) {
-        const dt = DAYS.findIndex((day) => day === dt);
-        if (dt > now.getDay()) {
-            d.setDate(now.getDate() + (dt - now.getDay()));
+        const day = DAYS.findIndex((item) => item === dt);
+        if (day > now.getDay()) {
+            d.setDate(now.getDate() + (day - now.getDay()));
         } else {
-            d.setDate(now.getDate() + (7 - now.getDay()) + dt);
+            d.setDate(now.getDate() + (7 - now.getDay()) + day);
         }
     } else if (dt.isNumeric()) {
         if (dt > now.getDate()) {
@@ -202,26 +114,23 @@ function parseArgs() {
             }
         },
         complete: function () {
-            let arg;
-            options.itemIds = [];
-            while ((arg = args.shift())) {
-                options.itemIds.push(parseInt(arg, 36));
-            }
+            options.ids = args.map((id) => parseInt(id, 36));
         },
         list: function () {
+            options.list = {};
             if (args.length === 0) {
-                options.listType = "all";
+                options.list.type = "all";
                 return;
             }
 
             let arg = args.shift();
 
             if (["dated", "tagged"].includes(arg.toLowerCase()) && args.length > 0) {
-                options.listType = arg.toLowerCase();
-                options.listSearch = args.shift();
+                options.list.type = arg.toLowerCase();
+                options.list.search = args.shift();
             } else {
-                options.listType = "named";
-                options.listSearch = arg;
+                options.list.type = "named";
+                options.list.search = arg;
             }
         }
     };
@@ -241,23 +150,17 @@ function parseArgs() {
 }
 
 function displayTasks(items) {
-    function dueLength(a, b) {
-        const aLength = (a.due) ? a.due.toDateString().length : 0;
-        const bLength = (b.due) ? b.due.toDateString().length : 0;
-        return Math.max(aLength, bLength);
-    }
-
     const maxLengths = {
-        id: items.reduce((a, b) => Math.max(a.id.toString().length, b.id.toString().length)),
-        name: items.reduce((a, b) => Math.max(a.name.length, b.name.length)),
-        due: items.reduce(dueLength),
-        tags: items.reduce((a, b) => Math.max(a.tags.join(", ").length, b.tags.join(", ").length))
+        id:   items.map((item) => item.id.toString().length).max(),
+        name: items.map((item) => item.name.length).max(),
+        due:  items.map((item) => (item.due) ? item.due.toDateString().length : 0).max(),
+        tags: items.map((item) => item.tags.join(", ").length).max(),
     };
 
     const h = {
-        id: "id".padLeft(maxLengths.id),
+        id:   "id".padLeft(maxLengths.id),
         name: "Task".padRight(maxLengths.name),
-        due: "Due".padRight(maxLengths.due),
+        due:  "Due".padRight(maxLengths.due),
         tags: "Tags".padRight(maxLengths.tags)
     };
 
@@ -280,7 +183,7 @@ try {
     switch (options.action) {
     case "add": {
         let attrs = { name: options.name };
-        let task = Task(attrs);
+        let task = Task.new(attrs);
 
         if (options.due) {
             task.due = parseDueDate(options.due);
@@ -296,13 +199,13 @@ try {
             task.tags = options.tags;
         }
 
-        console.log(`Added task ${task.name}!`);
+        console.log(`Added task ${task.name}.`);
         Task.registry.save();
         break;
     }
     case "delete": // fallthrough
     case "complete": {
-        const tasks = Task.registry.items.filter((item) => options.itemIds.includes(item.id));
+        const tasks = Task.registry.items.filter((item) => options.ids.includes(item.id));
         tasks.forEach(function (task) {
             if (options.action === "complete") {
                 task.complete();
@@ -315,16 +218,21 @@ try {
         break;
     }
     case "status": {
-        const now = Date.now();
-        const items = Task.registry.items.filter((t) => (t.due && t.due < now && !t.completed));
+        let today = new Date();
+        today.setHours(23);
+        today.setMinutes(59);
+        today.setSeconds(59);
+        today.setMilliseconds(999);
+        let items = Task.registry.items.filter((t) => (t.due && t.due < today && !t.completed));
+        items.sort((a, b) => a.id > b.id);
         console.log(items.map((t) => `[${t.id.toString(36)}] ${t.name}`).join(" "));
         break;
     }
     case "list": {
         let items;
-        const search = (options.listSearch) ? options.listSearch.toLowerCase() : "";
+        const search = (options.list.search) ? options.list.search.toLowerCase() : "";
 
-        switch (options.listType) {
+        switch (options.list.type) {
         case "dated": {
             let find = new Date(search);
             items = Task.registry.items.filter((item) => (item.due && item.due.toDateString() === find.toDateString()));
@@ -344,10 +252,10 @@ try {
             break;
         }
 
+        items.sort((a, b) => a.id > b.id);
         displayTasks(items.filter((item) => !item.completed));
     }
     }
 } catch (error) {
     console.log(error.message);
 }
-
